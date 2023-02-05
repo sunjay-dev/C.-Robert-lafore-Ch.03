9ca92ee012c0b5fb99462fa45e3f8dea0adda4cd
Sunjay Kumar <111847625+ak-ay675@users.noreply.github.com>
2023-02-05 14:07:26 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,22 @@
+	#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+int t=0;
+	float intial , year , rate , total;
+	cout<<"Enter intial amount: ";
+	cin>>intial;
+	cout<<"Enter ending amount: ";
+	cin>>end;
+	cout<<"Enter intrest rate (percent per year): ";
+	cin>>rate;
+	rate=rate/100;
+	do{
+		total=intial+(intial*rate);
+		
+		intial=total;
+		t++;
+	}while(end>=total);
+	cout<<t;
+  return 0;
+}
